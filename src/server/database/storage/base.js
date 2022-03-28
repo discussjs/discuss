6d15d4b939da1c927be6d502d8d5dc1d1a9a1590
Lete114 <me@imlete.cn>
2022-03-28 11:54:58 +0800

    Version 0.2.8 新增: 自定义数据库表名

--- src/server/database/storage/base.js
+++ src/server/database/storage/base.js
@@ -1,7 +1,9 @@
+const { DISCUSS_DB_ADMIN, DISCUSS_DB_COMMENT, DISCUSS_DB_COUNTER } = process.env
+
 module.exports = (type, options) => {
   const Model = require('dittorm')(type)
-  const Admin = new Model('d_admin', options)
-  const Comment = new Model('d_comment', options)
-  const Counter = new Model('d_counter', options)
+  const Admin = new Model(DISCUSS_DB_ADMIN || 'd_admin', options)
+  const Comment = new Model(DISCUSS_DB_COMMENT || 'd_comment', options)
+  const Counter = new Model(DISCUSS_DB_COUNTER || 'd_counter', options)
   return { Admin, Comment, Counter }
 }
