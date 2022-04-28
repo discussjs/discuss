7c420b8349181b4a5d9dcb0afdb6bbe4f0268381
Lete114 <me@imlete.cn>
2022-04-28 12:06:46 +0800

    refactor: 端口优先级 DISCUSS_PORT > PORT > 6870

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,9 @@
 const http = require('http')
 const main = require('./main')
 
-let PORT = process.env.PORT || process.env.DISCUSS_PORT || 6870
+const PORT = process.env.DISCUSS_PORT || process.env.PORT || 6870
 
-async function init() {
+function init() {
   const server = http.createServer(main)
   server.listen(PORT, () => {
     // eslint-disable-next-line no-console
