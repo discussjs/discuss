04c19a4d7eed90656f49785aca575f2afe2d33e7
Lete114 <me@imlete.cn>
2024-05-02 22:21:05 +0800

    chore: use fetch send request

--- src/client/lib/request.js
+++ src/client/lib/request.js
@@ -1,27 +1,11 @@
-function isJSON(t) {
-  try {
-    return JSON.parse(t)
-  } catch (error) {
-    return t
-  }
-}
-
 export default (options) => {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest()
-    xhr.open(options.method || 'POST', options.url, true)
-    if (options.method === 'GET') xhr.send()
-    else xhr.send(JSON.stringify(options.data))
-    xhr.onreadystatechange = () => {
-      try {
-        if (xhr.readyState === 4) {
-          const isSuccess = xhr.status >= 200 && xhr.status < 300
-          if (isSuccess) resolve(isJSON(xhr.responseText))
-          else reject(xhr)
-        }
-      } catch (error) {
-        reject(error)
-      }
-    }
-  })
+  options.method = options.method || 'POST'
+  const requestOptions = {
+    method: options.method,
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    body: options.method === 'GET' ? undefined : JSON.stringify(options.data)
+  }
+  return fetch(options.url, requestOptions).then((r) => r.json())
 }
